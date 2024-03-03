c2af3ebeabfe2156a77430f520f6d5972889c93d
4RohanSinha <rohansinha1@yahoo.com>
2024-03-03 15:01:03 -0800

    update changes

---
+++ Project Plant/Controllers/CoreDataStackViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStackViewController.swift
+//  Project Plant
+//
+//  Created by Rohan Sinha on 3/3/24.
+//
+
+import UIKit
+
+class CoreDataStackViewController: UIViewController {
+
+    var dataController: DataController?
+
+}
